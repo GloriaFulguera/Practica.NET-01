97a7c58d5749792481199b89f6901988e33cfd04
Gloria Fulguera <gloriafulguera@gmail.com>
2024-03-11 16:58:20 -0300

    Primera vista y correccion de conexion con base de datos

---
+++ CRUD-CORE-MVC/CRUD-CORE-MVC/Controllers/MantenedorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD_CORE_MVC.Datos;
+using CRUD_CORE_MVC.Models;
+
+namespace CRUD_CORE_MVC.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos _ContactoDatos=new ContactoDatos();
+        public IActionResult Listar()
+        {
+            var oLista=_ContactoDatos.Listar();
+            return View(oLista);
+        }
+    }
+}
---
+++ CRUD-CORE-MVC/CRUD-CORE-MVC/Datos/ContactoDatos.cs
@@ -0,0 +1,72 @@
+﻿using CRUD_CORE_MVC.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CRUD_CORE_MVC.Datos
+{
+    public class ContactoDatos
+    {
+
+        public List<ContactoModel> Listar()
+        {
+            var oLista = new List<ContactoModel>();
+
+            //instancia de Clase conexion para poder utilizar el metodo que obtiene la conexion
+            var cn = new Conexion();
+
+            //Conexion a BD
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                //Ejecuto el procedure de la Base de Datos
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //Leer cada registro de la tabla
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel()
+                        {
+                            idContacto = Convert.ToInt32(dr["idContacto"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Email = dr["Email"].ToString()
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+
+        public ContactoModel Obtener(int idContacto)
+        {
+            var oContacto = new ContactoModel();
+
+            //instancia de Clase conexion para poder utilizar el metodo que obtiene la conexion
+            var cn = new Conexion();
+
+            //Conexion a BD
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                //Ejecuto el procedure de la Base de Datos
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
+                cmd.Parameters.AddWithValue("idContacto", idContacto);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //Leer el registro de la tabla o lo que devuelve el SP
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oContacto.idContacto = Convert.ToInt32(dr["idContacto"]);
+                        oContacto.Nombre = dr["Nombre"].ToString();
+                        oContacto.Telefono = dr["Telefono"].ToString();
+                        oContacto.Email = dr["Email"].ToString();
+                    }
+                }
+            }
+            return oContacto;
+        }
+    }
+}
