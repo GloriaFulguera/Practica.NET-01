4cd5e4de7cc06f8841d29dedd515ec4b8aca6122
Gloria Fulguera <gloriafulguera@gmail.com>
2024-03-11 14:06:26 -0300

    Configuracion Nugets y conexion a BD con ADO NET

---
+++ CRUD-CORE-MVC/CRUD-CORE-MVC/Datos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿//Utiliza conexion ADO.NET
+using System.Data.SqlClient;
+
+namespace CRUD_CORE_MVC.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSQL = string.Empty;
+
+        public Conexion()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            cadenaSQL = builder.GetSection("ConnectionStrings:CadenaSQL").Value;
+        }
+        public string getCadenaSQL()
+        {
+            return cadenaSQL;
+        }
+    }
+}
---
+++ CRUD-CORE-MVC/CRUD-CORE-MVC/Models/ContactoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_CORE_MVC.Models
+{
+    public class ContactoModel
+    {
+        public int idContacto { get; set; }
+        public string? Nombre { get; set; }
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+    }
+}
