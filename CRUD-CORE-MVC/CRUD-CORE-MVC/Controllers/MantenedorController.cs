9c7e4459a3a5b853144bf8a61f79b6cfba923e01
Gloria Fulguera <gloriafulguera@gmail.com>
2024-03-13 13:14:43 -0300

    creacion del controlador principal y las vistas referentes a las acciones del mismo, vistas creadas a partir de layout como plantilla

--- CRUD-CORE-MVC/CRUD-CORE-MVC/Controllers/MantenedorController.cs
+++ CRUD-CORE-MVC/CRUD-CORE-MVC/Controllers/MantenedorController.cs
@@ -6,11 +6,63 @@ namespace CRUD_CORE_MVC.Controllers
 {
     public class MantenedorController : Controller
     {
-        ContactoDatos _ContactoDatos=new ContactoDatos();
+        ContactoDatos _ContactoDatos = new ContactoDatos();
         public IActionResult Listar()
         {
-            var oLista=_ContactoDatos.Listar();
+            //Mostrar lista de contactos
+            var oLista = _ContactoDatos.Listar();
             return View(oLista);
+        }
+        public IActionResult Guardar()
+        {
+            //Mostrar formulario
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+            //Accion Guardar
+            if (!ModelState.IsValid)
+                return View();
+            var rta = _ContactoDatos.Guardar(oContacto);
+            if (rta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+        public IActionResult Editar(int idContacto)
+        {
+            //Mostrar formulario editar
+            var oContacto = _ContactoDatos.Obtener(idContacto);
+            return View(oContacto);
+        }
+        [HttpPost]
+        public IActionResult Editar(ContactoModel oContacto)
+        {
+            //Accion Editar-Guardar
+            if (!ModelState.IsValid)
+                return View();
+            var rta = _ContactoDatos.Editar(oContacto);
+            if (rta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+        public IActionResult Eliminar(int idContacto)
+        {
+            //Mostrar formulario editar
+            var oContacto = _ContactoDatos.Obtener(idContacto);
+            return View(oContacto);
+        }
+        [HttpPost]
+        public IActionResult Eliminar(ContactoModel oContacto)
+        {
+            //Accion Editar-Guardar
+            var rta = _ContactoDatos.Eliminar(oContacto.idContacto);
+            if (rta)
+                return RedirectToAction("Listar");
+            else
+                return View();
         }
     }
 }
